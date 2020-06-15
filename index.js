0a957585ff405b767c6beb2f16334b34f1f06faa
heapwolf <paolo@async.ly>
2020-06-15 09:39:48 +0200

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = function prettyBytes (bytes) {
+  bytes = Number(bytes)
+
+  const divider = 1000
+  const magnitude = Math.log(bytes) / Math.log(divider) | 0
+  const result = (bytes / Math.pow(divider, magnitude))
+  const fixed = result.toFixed(2)
+
+  const suffix = magnitude
+    ? ('kMGTPEZY'[magnitude - 1]) + 'B' : 'B'
+
+  return {
+    suffix: suffix,
+    magnitude: magnitude,
+    result: result,
+    fixed: fixed
+  }
+}
